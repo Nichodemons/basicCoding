89595eb363b7b6af7d3f869198a72ecd2a76b3ab
Nichodemons <Nicholaj.p.a@gmail.com>
2025-09-29 11:33:27 +0200

    Update Program.cs

--- nicholaj/masterMind/Program.cs
+++ nicholaj/masterMind/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
 
 namespace masterMind
 {
@@ -10,21 +11,53 @@ namespace masterMind
             Console.WriteLine("The Computer will generate 4 random numbers between 1 - 2");
             Console.WriteLine("Numbers can repeat. An example: 2-2-1-2");
             bool isPlaying = true;
-            int[] arrayToGuess = new int[4];
+            int rndNumber1 = new Random().Next(1, 4);
+            int rndNumber2 = new Random().Next(1, 4);
+            int rndNumber3 = new Random().Next(1, 4);
+            int rndNumber4 = new Random().Next(1, 4);
+
+            int[] arrayGenerated = new int[] {rndNumber1, rndNumber2, rndNumber3, rndNumber4};
+
+            //write out array for testing purposes
+            for (int i = 0; i < arrayGenerated.Length; i++)
+            {
+                Console.Write(arrayGenerated[i]);
+            }
+            Console.WriteLine();
 
             while (isPlaying)
             {
+                string playerGuess = Console.ReadLine();
+                int[] playerConvertedArray[] = int.Parse(playerGuess);
 
-            for (int i = 0; i < arrayToGuess.Length; i++)
-            {
-                int rndNumber = new Random().Next(1, 4);
-                arrayToGuess[i] = rndNumber;
-                Console.Write(rndNumber);
-            }
-            
+                int result = 0;
 
-            
+                for (int i = 0; i < arrayGenerated.Length; i++)
+                {
+                    if (playerGuess[i] == arrayGenerated[i])
+                    {
+                        result = +1;
+                    }
+                    else
+                    {
+                        result = result;
+                    }
+                        }
+                    Console.WriteLine("you have " + result + " correct");
+                    result = 0;
+                    Console.WriteLine("type q to quit or anything else to continue");
+
+                    if (Console.ReadLine() == "q")
+                    {
+                        isPlaying = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Guess again");
+                    }
+
+                }
+                Console.ReadLine();
             }
         }
     }
-}
