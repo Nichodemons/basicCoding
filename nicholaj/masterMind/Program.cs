69e8d85af08f9eb29f13d4aa17f5af478f17ab8f
Nichodemons <Nicholaj.p.a@gmail.com>
2025-09-29 10:14:12 +0200

    added MasterMind to nichos folder

---
+++ nicholaj/masterMind/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography;
+
+namespace masterMind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to MasterMind");
+            Console.WriteLine("The Computer will generate 4 random numbers between 1 - 2");
+            Console.WriteLine("Numbers can repeat. An example: 2-2-1-2");
+            bool isPlaying = true;
+            int[] arrayToGuess = new int[4];
+
+            while (isPlaying)
+            {
+
+            for (int i = 0; i < arrayToGuess.Length; i++)
+            {
+                int rndNumber = new Random().Next(1, 4);
+                arrayToGuess[i] = rndNumber;
+                Console.Write(rndNumber);
+            }
+            
+
+            
+            }
+        }
+    }
+}
